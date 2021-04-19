566e0ad7aaebbefaa59231a5aa3d1213cc7ab7ef
Bagus Abdul Kurniawan <33747231+BagusAK95@users.noreply.github.com>
2021-04-19 12:33:46 +0700

    Feat: Set API Docs (#25)
    * set api docs
    
    * change title
    
    * add feature flag
    
    * add swagger path config
    
    Co-authored-by: BagusAK95 <bagus@kata.ai>

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("./lib/router");
 const getfn = require("./lib/getfn");
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 module.exports = function (merapi) {
 
@@ -26,6 +28,22 @@ module.exports = function (merapi) {
                 let bodyParserOptions = cfg.bodyParser || {};
                 let middleware = cfg.middleware || [];
                 let routes = cfg.routes || {};
+                let swaggerEnable = (cfg.swagger && cfg.swagger.enable) ? cfg.swagger.enable : false
+                if (swaggerEnable) {
+                    let swaggerPath = cfg.swagger.path || '/api-docs';
+                    let swaggerInfo = cfg.swagger.info || {
+                        title: 'API Documentation',
+                        version: '1.0.0',
+                    };
+                    let swaggerSpec = swaggerJsdoc({
+                        definition: {
+                          openapi: '3.0.0',
+                          info: swaggerInfo,
+                        },
+                        apis: ['./components/controllers/*_controller.ts', './components/managers/*_manager.ts', './components/swagger/*.yaml'],
+                    });
+                    app.use(swaggerPath, swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+                }
 
                 if (bodyParserOptions.verify) {
                     bodyParserOptions.verify = yield getFn(bodyParserOptions.verify);
